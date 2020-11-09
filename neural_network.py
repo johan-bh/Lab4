1fc746c53aec253eed02d1bbffc27a8f33d6f258
Johan Böcher Hanehøj <jbhanehoj@gmal.com>
2020-11-09 12:15:50 +0100

    Refac: updated version of dataload

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # create Neural Network to classify benign og malignant breast cancer in women
 
@@ -9,13 +10,14 @@ data = load_breast_cancer()
 
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['target'] = pd.Series(data.target)
-x = df
+x = df.drop(["target"],axis=1)
+
 y = df['target']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=85)
+scaler = StandardScaler()
+trans_data = scaler.fit_transform(x_train)
 
 
-"""
 dimensions_in = x_train.shape[1]
 dimensions_out = 1
 layer_dimensions = [dimensions_in, 25, 10, dimensions_out]
-"""
