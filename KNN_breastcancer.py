82f3f5d5aa4df5b1ace50f5845d993e9dc64682e
August Tollerup <augusttollerup@Augusts-MacBook-Pro.local>
2020-11-09 11:42:58 +0100

    Init

--- KNN_breastcancer.py
+++ KNN_breastcancer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
 from sklearn import datasets
@@ -7,22 +8,32 @@ from sklearn.neighbors import KNeighborsClassifier
 
 cancer_data = load_breast_cancer()
 
+H = 5
+
 #Prints the dataset's feature names
-#print((cancer_data['feature_names']))
+# print((cancer_data['feature_names']))
+
+device = torch.device('cpu')
 
 #Converts the Scikit-Learn Bunch object to a Pandas DF
 cancer_df = pd.DataFrame(cancer_data.data, columns=cancer_data.feature_names)
 cancer_df['target'] = pd.Series(cancer_data.target)
 
+# print(cancer_df)
+
 #Store data in X,y
 X = cancer_df[cancer_df.columns.drop('target')]
 y = cancer_df['target']
 
+# print(X)
+x = torch.tensor(np.expand_dims(X,1), dtype=torch.float32, device=device)
+# print(y)
+
 #Create train/test split
-X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=0)
 
 #Create KNN-model and set no. of neighbors to 1
-KNN = KNeighborsClassifier(n_neighbors=1)
+KNN = KNeighborsClassifier(n_neighbors=13)
 
 #Train the model on the training data
 KNN.fit(X_train, y_train)
