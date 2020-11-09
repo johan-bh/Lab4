1a234837f1f85da4448d4bbe605277010971516e
Johan Böcher Hanehøj <jbhanehoj@gmal.com>
2020-11-09 21:28:24 +0100

    Refac: Neural Network attempt. Couldn't achieve an accuracy above 70%...

--- KNN_breastcancer.py
+++ KNN_breastcancer.py
@@ -13,21 +13,6 @@ cancer_data = load_breast_cancer()
 #Converts the Scikit-Learn Bunch object to a Pandas DF
 cancer_df = pd.DataFrame(cancer_data.data, columns=cancer_data.feature_names)
 cancer_df['target'] = pd.Series(cancer_data.target)
-print(import numpy as np
-from sklearn.datasets import load_breast_cancer
-import pandas as pd
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-
-cancer_data = load_breast_cancer()
-
-#Prints the dataset's feature names
-#print((cancer_data['feature_names']))
-
-#Converts the Scikit-Learn Bunch object to a Pandas DF
-cancer_df = pd.DataFrame(cancer_data.data, columns=cancer_data.feature_names)
-cancer_df['target'] = pd.Series(cancer_data.target)
 
 #Store data in X,y
 X = cancer_df[cancer_df.columns.drop('target')]
@@ -46,7 +31,6 @@ KNN.fit(X_train, y_train)
 #print(KNN.predict(X_test))
 
 #Test the KNN-model on the test data and print the test accuracy
-print(KNN.score(X_test,y_test)))
 #Store data in X,y
 X = cancer_df[cancer_df.columns.drop('target')]
 y = cancer_df['target']
--- neural_network.py
+++ neural_network.py
@@ -1,80 +1,109 @@
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
+import numpy as np
 import torch
 from torch.autograd import Variable
 import torch.utils.data as data_utils
 import torch.nn.init as init
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+import operator
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import numpy as np
-from pprint import pprint
 import matplotlib.pyplot as plt
 
-# create Neural Network to classify benign og malignant breast cancer in women
+#               Create Neural Network to classify benign og malignant breast cancer in women
 
-device = torch.device('cpu')
+# pd.set_option('display.max_columns', None)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
 
 # Load the data
 data = load_breast_cancer()
+x = pd.DataFrame(data.data, columns=data.feature_names)
+y = np.array(data.target)
+# Create train/test split (80/20) using random_state to create replicable results
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
 
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df['target'] = pd.Series(data.target)
-
-y = df['target'].to_numpy()
-x = df.drop(["target"],axis=1)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=85)
-
+# Initiate scaler used for feature standardization
 scaler = StandardScaler()
-transformed_train = scaler.fit_transform(x_train)
 
-train = data_utils.TensorDataset(torch.from_numpy(transformed_train).float(),
-                                 torch.from_numpy(y_train).float())
+# Scale and transform training data
+transformed = scaler.fit_transform(x_train)
+train = data_utils.TensorDataset(torch.from_numpy(transformed).float(),torch.from_numpy(y_train).float())
 dataloader = data_utils.DataLoader(train, batch_size=128, shuffle=False)
 
-transformed_test = scaler.fit_transform(x_test)
-test_set = torch.from_numpy(transformed_test).float()
+# Scale and transform test data
+transformed = scaler.fit_transform(x_test)
+test_set = torch.from_numpy(transformed).float()
 test_valid = torch.from_numpy(y_test).float()
-dimensions_in = x_train.shape[1]
-dimensions_out = 1
-layer_dimensions = [dimensions_in, 25, 10, dimensions_out]
 
-x = torch.tensor(np.expand_dims(x_train,1), dtype=torch.float32, device=device)
-y = torch.tensor(np.expand_dims(y_train,1), dtype=torch.float32, device=device)
+# Neural Network specifications
+H = 10
+dimemsions_in = x_train.shape[1]
+dimemsions_out = 1
 
-H = 5
-
+# The Neural Network
 model = torch.nn.Sequential(
-    torch.nn.Linear(30, H),
+    torch.nn.Linear(dimemsions_in, H),
     torch.nn.ReLU(),
     torch.nn.Linear(H, H),
     torch.nn.ReLU(),
     torch.nn.Linear(H, H),
     torch.nn.ReLU(),
-    torch.nn.Linear(H,1),
-)
-model.to(device)
-loss_fn = torch.nn.MSELoss(reduction='mean')
+    torch.nn.Linear(H, dimemsions_out),)
 
-learning_rate = 1e-3
+# Optimizer, learning rate, loss function and number of iterations
+loss_fn = torch.nn.MSELoss(reduction='mean')
+learning_rate = 0.0004
+num_epochs = 300
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-T = 5000
+# Run the NN and track accuracy, loss, etc. throughout each epoch.
+nn_log = {"loss": [], "accuracy": [], "loss_value": [], "accuracy_value": []}
+for epoch in range(num_epochs):
+    loss = None
+    for index, (batch, target) in enumerate(dataloader):
+        y_pred = model(Variable(batch))
+        loss = loss_fn(y_pred, Variable(target.float()))
+        prediction = [1 if x > 0.5 else 0 for x in y_pred.data.numpy()]
+        correct = (prediction == target.numpy()).sum()
+        y_val_pred = model(Variable(test_set))
+        loss_value = loss_fn(y_val_pred, Variable(test_valid.float()))
+        prediction_value = [1 if x > 0.5 else 0 for x in y_val_pred.data.numpy()]
+        correct_val = (prediction_value == test_valid.numpy()).sum()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-Loss = np.zeros(T)
+    # Append metrics for each epoch
+    nn_log["loss"].append(loss.item())
+    nn_log["accuracy"].append(100 * correct / len(prediction))
+    nn_log["loss_value"].append(loss_value.item())
+    nn_log["accuracy_value"].append(100 * correct_val / len(prediction_value))
 
-for t in range(T):
-    y_pred = model(x)
+    # Print the metrics for each epoch
+    print("Loss, accuracy, val loss, val acc at epoch", epoch + 1, nn_log["loss"][-1],
+          nn_log["accuracy"][-1], nn_log["loss_value"][-1], nn_log["accuracy_value"][-1])
 
-    loss = loss_fn(y_pred, y)
-    Loss[t] = loss.item()
+# Find the highest accuracy value
+index, value = max(enumerate(nn_log["accuracy_value"]), key=operator.itemgetter(1))
 
-    optimizer.zero_grad()
+print("Higgest accuracy score: {} at iteration: {}".format(value, index))
 
-    loss.backward()
+# Plot accuracy by epochs
+plt.plot(nn_log['accuracy'])
+plt.plot(nn_log['accuracy_value'])
+plt.title('Model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
 
-    optimizer.step() 
-
+# Plot loss by epochs
+plt.plot(nn_log['loss'])
+plt.plot(nn_log['loss_value'])
+plt.title('Model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
