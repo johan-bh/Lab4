e5f18aa3eada1897d878a2ebae9286e90305a214
Johan <jbhanehoj@gmail.com>
2020-11-09 11:39:44 +0100

    Feat: Converted Sklearn Bunch object to Dataframe and dropped irrelevant columns. Also commented a draft for determining input-output

--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,30 @@
 import torch
 import sklearn.datasets
-
+import pandas as pd
+import numpy as np
+import torch
+from torch.autograd import Variable
+import torch.utils.data as data_utils
+import torch.nn.init as init
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
 # create Neural Network to classify benign og malignant breast cancer in women
 
-cancer_data
+# Load the data
+data = load_breast_cancer()
+
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = pd.Series(data.target)
+
+drop_columns = data.drop(["id", "diagnosis", "Unnamed: 32"], axis=1)
+diagnosis = {"M": 1, "B": 0}
+y = data["diagnosis"].replace(diagnosis)
+
+
+"""
+dimensions_in = x_train.shape[1]
+dimensions_out = 1
+layer_dimensions = [dimensions_in, 25, 10, dimensions_out]
+"""
